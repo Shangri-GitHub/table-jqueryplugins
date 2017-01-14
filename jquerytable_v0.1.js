8eb4bcb316f882b119fb8ad2568334af904fddd5
Shangri-la <467527309@qq.com>
2017-01-14 12:38:43 +0800

    add html and js

---
+++ jquerytable_v0.1.js
@@ -0,0 +1,29 @@
+/**
+ * Created by shangri-la on 14/01/2017.
+ */
+(function ($) {
+    $.fn.table = function (options) {
+        var defaults = {
+            //各种参数,各种属性
+            oddRow:'oddRow',   //奇数行的样式
+            evenRow:'evenRow',  //偶数行的样式
+            currentRow:'currentRow', //当前的样式
+            mouseType:'mouseover',
+        }
+        var options = $.extend(defaults, options);
+
+        this.each(function () {
+            //实现功能的代码
+            $(this).find('tr:odd').addClass(options.oddRow)     //奇数行变颜色
+            $(this).find('tr:even').addClass(options.evenRow)   // 偶数行变颜色
+
+            $(this).find('tr').bind(options.mouseType,function () {    
+                $(this).addClass(options.currentRow)
+            })
+            $(this).find('tr').bind('mouseout',function () {
+                $(this).removeClass(options.currentRow)
+            })
+
+        })
+    }
+})(jQuery)
